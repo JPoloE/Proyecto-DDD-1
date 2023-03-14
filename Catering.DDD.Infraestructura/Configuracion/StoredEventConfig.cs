854bf59fd729b0ce75c9c8190a8fc4be95a9eeb0
Jesus <jesusdavidpoloespitia@gmail.com>
2023-03-14 01:32:59 -0500

    Se crean las relaciones entre proyectos
     Se hicieron las referencias entre cada parte del proyecto

---
+++ Catering.DDD.Infraestructura/Configuracion/StoredEventConfig.cs
@@ -0,0 +1,21 @@
+﻿using Catering.DDD.Dominio.Generics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catering.DDD.Infraestructura.Configuracion
+{
+    public class StoredEventConfig : IEntityTypeConfiguration<StoredEvent>
+    {
+        public void Configure(EntityTypeBuilder<StoredEvent> builder)
+        {
+            builder.ToTable("storedEvent");
+
+            builder.HasKey(p => p.StoredId);
+        }
+    }
+}
